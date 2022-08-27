12ba933eec137461b8295620ccad0bebc89cba7f
Dongyeong Kim <dongyeongkim33@gmail.com>
2022-08-27 19:06:03 +0000

    nice image

--- keyvalue/visualizer.py
+++ keyvalue/visualizer.py
@@ -35,7 +35,7 @@ def visualizer(inp, lab, model, model_type, leng):
     # Put the result into a color plot
     Z = Z.reshape(xx.shape)
 
-    ax.contourf(xx, yy, Z, cmap=plt.cm.Pastel2)
+    ax.contourf(xx, yy, Z, levels=32, cmap=plt.cm.Pastel2)
     ax.axis('off')
     
     
@@ -149,31 +149,32 @@ def visualizer_without_scat_VQ(inp, lab, model, model_type, leng):
 
 
 def visualizer_KV(inp, lab, model, model_type, leng):
-    colorbook = [cmap(0),cmap(1/7),cmap(2/7),cmap(3/7),cmap(4/7),cmap(5/7),cmap(6/7), cmap(1)]
     colorcode = []
+    colorbook = [cmap(0),cmap(1/7),cmap(2/7),cmap(3/7),cmap(4/7),cmap(5/7),cmap(6/7), cmap(1)]
     X = inp.cpu().numpy()
     Y = lab.cpu().numpy()
+    for i in Y:
+        colorcode.append(colorbook[i])
+    
+    
+
 
     fig, ax = plt.subplots()
     typ = int(model_type[leng:])
-    for i in Y:
-            colorcode.append(colorbook[i])
-    
     h = .02  # step size in the mesh
     x_min, x_max = 0, 8
     y_min, y_max = 0, 8
     xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
     positions = torch.Tensor(np.vstack(list(zip(xx.ravel(), yy.ravel())))).cuda()
-    Z = model(positions)
-    Z = Z.cpu().detach()
-    Z = np.argmax(Z, axis=1)
+    Z = model(positions).softmax(dim=1)
+    Z = torch.argmax(Z, dim=1)
+    Z = Z.cpu().detach().numpy()
     Z = Z.reshape(xx.shape)
 
-    ax.contourf(xx, yy, Z, cmap=plt.cm.Pastel2)
-    ax.axis('off')
+    ax.imshow(Z, cmap=plt.cm.Pastel2, vmin=0, vmax=7)
     
     # Plot also the training points
-    ax.scatter(X[:, 0], X[:, 1], c=colorcode, edgecolors='black')
+    ax.scatter(50*X[:, 0], 50*X[:, 1], c=colorcode, edgecolors='black')
     ax.set_title(model_type)
     fig.savefig(model_type+".png")
     plt.close()
@@ -202,14 +203,14 @@ def visualizer_without_scat_KV(inp, lab, model, model_type, leng):
     
 
     # Put the result into a color plot
-    Z = model(positions)
+    Z = model(positions).softmax(dim=1)
     Z = Z.cpu().detach()
     Z = np.argmax(Z, axis=1)
     Z = Z.reshape(xx.shape)
 
-    ax.contourf(xx, yy, Z, cmap=plt.cm.Pastel2)
+    ax.imshow(Z, cmap=plt.cm.Pastel2, vmin=0, vmax=7)
     ax.axis('off')
     
     ax.set_title(model_type)
     fig.savefig(model_type+".png")
-    plt.close()
+    #plt.close()
